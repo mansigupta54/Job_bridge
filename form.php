e995a16d02d7a414c400acfcd3a5ddc6d631f2ce
Gaurav Sharma <grvsharma324@gmail.com>
2020-11-12 20:28:59 +0530

    form

--- form.php
+++ form.php
@@ -16,14 +16,11 @@ $password = $_POST['password'];
 $phone = $_POST['phone']
 $sans = $_POST['Securityquestion'];
 $gender = $_POST['gender'];
-<<<<<<< HEAD
 $sql = "INSERT INTO job-bridge(first_name,last_name,e-mail,passcode,phone,security_ans,gender) VALUES 
 ('$fname','$lname','$email','$password','$phone','$sans','$gender')";
-=======
 }
 $sql = "INSERT INTO job-bridge(first_name,last_name,e-mail,password,security_ans,gender) VALUES 
 ('$fname','$lname','$email','$password','$s_ques','$gender')";
->>>>>>> 63dd972a02b195dfd9c38cf632fa49d5ca8e0975
 if(!mysqli_query($con,$sql))
 {
     echo 'Not inserted';
