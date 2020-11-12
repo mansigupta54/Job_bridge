4e63bb0ca726abf54c5287b281e951a436caa567
Gaurav Sharma <grvsharma324@gmail.com>
2020-11-12 20:09:44 +0530

    form

--- form.php
+++ form.php
@@ -8,12 +8,14 @@ if(!mysqli_select_db($con,'job-bridge'))
 {
     echo"Database not selected";
 }
+if(isset($_POST['submit'])){
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 $s_ques = $_POST['Securityquestion'];
 $gender = $_POST['gender'];
+}
 $sql = "INSERT INTO job-bridge(first_name,last_name,e-mail,password,security_ans,gender) VALUES 
 ('$fname','$lname','$email','$password','$s_ques','$gender')";
 if(!mysqli_query($con,$sql))
@@ -23,5 +25,5 @@ if(!mysqli_query($con,$sql))
 else{
     echo 'Data inserted';
 }
-header("refresh:3;url=index1.html")
+header("refresh:3;url=index1.html");
 ?>
