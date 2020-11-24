e693985f65397f9372541e7acdf88e5f2b884b76
Gaurav Sharma <grvsharma324@gmail.com>
2020-11-24 15:40:26 +0530

    login

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+$con = mysqli_connect('localhost','root','','job-bridge');
+if(!$con)
+{
+    echo"Not connect to the server";
+}
+if(!mysqli_select_db($con,'job-bridge'))
+{
+    echo"Database not selected";
+}
+$email = $_POST["uname"];
+$password = $_POST["psw"];
+$sql = "Select * from user where email='$email' AND password='$password'";
+$result = mysqli_query($con,$sql);
+$num = mysqli_num_rows($result);
+if($num == 1){
+    $login=true;
+    header("refresh:0;url=index1.html");
+}
+else{
+    $showError = "Invalid Credentials";
+}
+
+?>
